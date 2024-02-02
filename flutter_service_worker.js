10039f4c6428c77247d7c8e676074898980ab731
Sung H Kim <hoony0916@gmail.com>
2024-02-02 05:37:25 -0800

    debugging message added

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "abb438ce2b69abb282636c6b5c19827a",
-"index.html": "992220613b62c032b659f49f85497c82",
-"/": "992220613b62c032b659f49f85497c82",
-"main.dart.js": "190b6898ad6f464637437b8a534c2a9b",
+"index.html": "cd41a03cfe238e7dfc653b5026fa56dd",
+"/": "cd41a03cfe238e7dfc653b5026fa56dd",
+"main.dart.js": "1dceab049bf6a826c69c7cd1f6049279",
 "flutter.js": "7d69e653079438abfbb24b82a655b0a4",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
