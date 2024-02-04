7e9c0f2e8d973f15462b0f0b6a3a6704773b575b
Sung H Kim <hoony0916@gmail.com>
2024-02-04 05:54:14 -0800

    프린트 기능 완결판 : +주거, 관심

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "abb438ce2b69abb282636c6b5c19827a",
-"index.html": "0b77451e5faf82dbcd861a9a46f4d02e",
-"/": "0b77451e5faf82dbcd861a9a46f4d02e",
-"main.dart.js": "677580231bde9ab836d3325a2a066e26",
+"index.html": "74f2535bfa7e1e8a54b9a5c9ed1aa2e6",
+"/": "74f2535bfa7e1e8a54b9a5c9ed1aa2e6",
+"main.dart.js": "5cd0264505bb614f21284291f8dea62a",
 "flutter.js": "7d69e653079438abfbb24b82a655b0a4",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
